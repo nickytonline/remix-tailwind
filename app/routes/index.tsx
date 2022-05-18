8323b196c3ea256560b6a608f39a130fae238585
Nick Taylor <nick@iamdeveloper.com>
2022-05-18 18:35:12 -0400

    chore: added tailwind

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,95 @@
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
+      <div className="absolute inset-0 bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]"></div>
+      <div className="relative bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
+        <div className="mx-auto max-w-md">
+          <div className="divide-y divide-gray-300/50">
+            <div className="space-y-6 py-8 text-base leading-7 text-gray-600">
+              <p>Welcome to Remix with Tailwind!</p>
+              <ul className="space-y-4">
+                <li className="flex items-center">
+                  <svg
+                    className="h-6 w-6 flex-none fill-sky-100 stroke-sky-500 stroke-2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  >
+                    <circle cx="12" cy="12" r="11" />
+                    <path
+                      d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9"
+                      fill="none"
+                    />
+                  </svg>
+                  <p className="ml-4">Now 110% fresher</p>
+                </li>
+                <li className="flex items-center">
+                  <svg
+                    className="h-6 w-6 flex-none fill-sky-100 stroke-sky-500 stroke-2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  >
+                    <circle cx="12" cy="12" r="11" />
+                    <path
+                      d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9"
+                      fill="none"
+                    />
+                  </svg>
+                  <p className="ml-4">
+                    Everyone including your grandma uses it!
+                  </p>
+                </li>
+                <li className="flex items-center">
+                  <svg
+                    className="h-6 w-6 flex-none fill-sky-100 stroke-sky-500 stroke-2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  >
+                    <circle cx="12" cy="12" r="11" />
+                    <path
+                      d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9"
+                      fill="none"
+                    />
+                  </svg>
+                  <p className="ml-4">It even vaccuums for you!</p>
+                </li>
+              </ul>
+            </div>
+            <div className="pt-8 text-base font-semibold leading-7">
+              <p className="text-gray-900">Want to dig deeper into Remix?</p>
+              <p>
+                <a
+                  href="https://remix.run/docs/en/v1"
+                  className="text-sky-500 hover:text-sky-600"
+                >
+                  Read the docs &rarr;
+                </a>
+              </p>
+            </div>
+            <div className="pt-8 text-base font-semibold leading-7">
+              <p className="text-gray-900">Want to dig deeper into Tailwind?</p>
+              <p>
+                <a
+                  href="https://tailwindcss.com/docs"
+                  className="text-sky-500 hover:text-sky-600"
+                >
+                  Read the docs &rarr;
+                </a>
+              </p>
+            </div>
+            <div className="pt-8 text-base font-semibold leading-7">
+              <p className="text-gray-900">Looking for the source code?</p>
+              <p>
+                <a
+                  href="https://github.com/nickytonline/remix-tailwind"
+                  className="text-sky-500 hover:text-sky-600"
+                >
+                  Grab it here &rarr;
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
